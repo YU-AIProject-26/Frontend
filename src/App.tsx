0e00096beb99d33daff194cdf498dcc5af8dc7ab
전유진 <123734118+zzzin18@users.noreply.github.com>
2026-04-05 16:05:39 +0900

    [추가] 프로젝트 초기 세팅

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+//import { useState } from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h1>Acta Frontend</h1>
+    </div>
+  );
+}
+
+export default App;
