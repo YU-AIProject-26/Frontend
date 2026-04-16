60c7d04b7f655194efe79166e3b49b282593488e
Woohyeon Roh <nwh0326@yu.ac.kr>
2026-04-16 17:59:29 +0900

    [추가] react-router-dom 기반 페이지 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
 import ThemeToggle from './components/ThemeToggle';
 
 export default function App() {
   return (
     <>
-      <LandingPage />
+      <Routes>
+        <Route path = "/" element = {<Navigate to = "/landing" replace />} />
+        <Route path = "/landing" element = {<LandingPage />} />
+        <Route path = "/auth/login" element = {<LoginPage />} />
+        <Route path = "*" element = {<Navigate to = "/landing" replace />} />
+      </Routes>
       <ThemeToggle />
     </>
   );
