997ae65b8e75977ab9c1eadddcc06e6955dc4a99
Woohyeon Roh <nwh0326@yu.ac.kr>
2026-04-21 03:30:37 +0900

    [수정] 비밀번호 재설정 페이지 라우팅 연결

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
+import PasswordResetPage from './pages/PasswordResetPage';
 import ThemeToggle from './components/ThemeToggle';
 import AuthLayout from './components/AuthLayout';
 
@@ -15,6 +16,7 @@ export default function App() {
         <Route element = {<AuthLayout />}>
           <Route path = "/auth/login" element = {<LoginPage />} />
           <Route path = "/auth/signup" element = {<SignupPage />} />
+          <Route path = "/auth/reset-password" element = {<PasswordResetPage />} />
         </Route>
 
         <Route path = "*" element = {<Navigate to = "/landing" replace />} />
